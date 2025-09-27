c884f14e0db3db261a201a37c709b82758e78667
Max Petite <mjpeti28@colby.edu>
2025-09-27 08:54:40 -0400

    roster updates

--- rosters.py
+++ rosters.py
@@ -1,21 +1,12 @@
 Fantasy_Rosters = {
 
-  "The_Little_Stinkers": {
-    "QB": ["Trevor Lawrence", "Justin Fields (IR)"],
-    "RB": ["Jordan Mason", "Chuba Hubbard", "RJ Harvey", "Cam Skattebo", "Bhayshul Tuten"],
-    "WR": ["Puka Nacua", "Garrett Wilson", "Brian Thomas Jr.", "Chris Olave", "Emeka Egbuka", "Josh Downs"],
-    "TE": ["George Kittle (IR)", "Brenton Strange"],
-    "DST": ["Packers"],
-    "K": ["Tyler Loop"]
-  },
-
-  "Hinesward_Jersey": {
-    "QB": ["Lamar Jackson", "Jordan Love"],
-    "RB": ["Chase Brown", "Jaylen Warren", "Omarion Hampton", "Quinshon Judkins"],
-    "WR": ["Justin Jefferson", "DJ Moore", "Calvin Ridley", "Stefon Diggs", "Troy Franklin", "Jayden Reed (IR)"],
-    "TE": ["Dalton Kincaid", "T.J. Hockenson"],
-    "DST": ["Eagles"],
-    "K": ["Cam Little"]
+  "Braeside_Bangers": {
+    "QB": ["Jalen Hurts", "Baker Mayfield"],
+    "RB": ["Derrick Henry", "James Cook", "Tyler Allgeier", "Tyjae Spears (IR)"],
+    "WR": ["Malik Nabers", "DeVonta Smith", "Rome Odunze", "Rashid Shaheed"],
+    "TE": ["Zach Ertz", "Evan Engram", "Jonnu Smith"],
+    "DST": ["Steelers", "Bengals"],
+    "K": ["Brandon Aubrey"]
   },
 
   "Dobis_PR": {
@@ -23,89 +14,98 @@ Fantasy_Rosters = {
     "RB": ["Jonathan Taylor", "Breece Hall", "TreVeyon Henderson", "Isiah Pacheco"],
     "WR": ["Ja'Marr Chase", "Tetairoa McMillan", "Quentin Johnston", "Khalil Shakir", "Jordan Addison", "Cedric Tillman"],
     "TE": ["Brock Bowers"],
-    "DST": ["Patriots"],
+    "DST": ["Chargers", "Patriots"],
     "K": ["Jason Myers"]
   },
 
-  "Braeside_Bangers": {
-    "QB": ["Jalen Hurts", "Baker Mayfield"],
-    "RB": ["Derrick Henry", "James Cook", "Tyler Allgeier", "Tyjae Spears (IR)"],
-    "WR": ["Malik Nabers", "DeVonta Smith", "Rome Odunze", "Rashid Shaheed"],
-    "TE": ["Zach Ertz", "Evan Engram", "Jonnu Smith"],
-    "DST": ["Bengals"],
-    "K": ["Brandon Aubrey"]
+  "JohnBurrows_School": {
+    "QB": ["Drake Maye"],
+    "RB": ["Saquon Barkley", "Josh Jacobs", "Kyren Williams", "Zach Charbonnet"],
+    "WR": ["Marvin Harrison Jr.", "George Pickens", "Travis Hunter", "Matthew Golden", "Jauan Jennings", "Wan'Dale Robinson", "Tre Tucker"],
+    "TE": ["Trey McBride"],
+    "DST": ["Seahawks"],
+    "K": ["Chris Boswell"]
+  },
+
+  "The_Little_Stinkers": {
+    "QB": ["Justin Fields"],
+    "RB": ["Jordan Mason", "Chuba Hubbard", "RJ Harvey", "Cam Skattebo", "Bhayshul Tuten"],
+    "WR": ["Puka Nacua", "Garrett Wilson", "Brian Thomas Jr.", "Chris Olave", "Emeka Egbuka", "Josh Downs"],
+    "TE": ["George Kittle (IR)", "Brenton Strange"],
+    "DST": ["Packers"],
+    "K": ["Tyler Loop"]
   },
 
   "Kirkwood_Kittens": {
     "QB": ["Josh Allen", "J.J. McCarthy"],
-    "RB": ["D'Andre Swift", "Kaleb Johnson", "Jacory Croskey-Merritt"],
+    "RB": ["D'Andre Swift", "Jacory Croskey-Merritt"],
     "WR": ["Jaxon Smith-Njigba", "Nico Collins", "Hollywood Brown", "Jerry Jeudy", "Rashee Rice", "Xavier Legette", "Jayden Higgins"],
     "TE": ["Travis Kelce"],
     "DST": ["Texans"],
     "K": ["Jake Bates"]
   },
 
-  "JohnBurrows_School": {
-    "QB": ["Drake Maye", "Aaron Rodgers"],
-    "RB": ["Saquon Barkley", "Josh Jacobs", "Kyren Williams", "Zach Charbonnet"],
-    "WR": ["Marvin Harrison Jr.", "George Pickens", "Travis Hunter", "Matthew Golden", "Jauan Jennings", "Wan'Dale Robinson"],
-    "TE": ["Trey McBride"],
-    "DST": ["Seahawks"],
-    "K": ["Chris Boswell"]
+  "KansasCity_Carnevores": {
+    "QB": ["Kyler Murray", "Jayden Daniels"],
+    "RB": ["Bijan Robinson", "Travis Etienne Jr.", "David Montgomery", "Tank Bigsby", "Rachaad White", "Jaylen Wright"],
+    "WR": ["Cooper Kupp", "Mike Evans", "Ricky Pearsall", "Xavier Worthy (IR)"],
+    "TE": ["Sam LaPorta"],
+    "DST": ["Vikings"],
+    "K": ["Chase McLaughlin"]
   },
 
   "Righteous_Gemstones": {
     "QB": ["Caleb Williams", "Brock Purdy"],
-    "RB": ["De'Von Achane", "James Conner", "Rhamondre Stevenson", "Jerome Ford"],
+    "RB": ["De'Von Achane", "Rhamondre Stevenson", "Jerome Ford"],
     "WR": ["Davante Adams", "CeeDee Lamb", "Terry McLaurin", "Jameson Williams", "Darnell Mooney", "Chris Godwin (IR)", "Brandon Aiyuk (IR)"],
     "TE": ["Jake Ferguson", "Juwan Johnson"],
-    "DST": ["Rams"],
+    "DST": ["Lions"],
     "K": ["Jake Elliott"]
   },
 
   "The_Lords_Force": {
-    "QB": ["Russell Wilson", "Bryce Young", "Joe Burrow (IR)"],
-    "RB": ["Bucky Irving", "Ashton Jeanty", "Javonte Williams", "Joe Mixon (IR)"],
+    "QB": ["Bryce Young", "Dak Prescott", "Joe Burrow (IR)"],
+    "RB": ["Bucky Irving", "Javonte Williams", "Ashton Jeanty", "Joe Mixon (IR)"],
     "WR": ["Amon-Ra St. Brown", "Zay Flowers", "Jaylen Waddle", "Michael Pittman Jr.", "Keenan Allen", "Marvin Mims Jr."],
     "TE": ["David Njoku", "Kyle Pitts"],
     "DST": ["Bills"],
     "K": ["Cairo Santos"]
   },
 
-  "KansasCity_Carnevores": {
-    "QB": ["Kyler Murray", "Jayden Daniels"],
-    "RB": ["Bijan Robinson", "Travis Etienne Jr.", "David Montgomery", "Tank Bigsby", "Rachaad White", "Jaylen Wright"],
-    "WR": ["Mike Evans", "Ricky Pearsall", "Cooper Kupp", "Xavier Worthy (IR)"],
-    "TE": ["Sam LaPorta"],
-    "DST": ["Vikings"],
-    "K": ["Chase McLaughlin"]
-  },
-
-  "BlueStreak_Bombers": {
-    "QB": ["Patrick Mahomes", "Jared Goff"],
-    "RB": ["Alvin Kamara", "Christian McCaffrey", "Aaron Jones (IR)", "Trey Benson"],
-    "WR": ["Tyreek Hill", "Deebo Samuel", "Tee Higgins", "Rashod Bateman"],
-    "TE": ["Hunter Henry", "Mark Andrews", "Dallas Goedert"],
-    "DST": ["Ravens"],
-    "K": ["Cameron Dicker"]
-  },
-
   "KWood_Super_Marios": {
     "QB": ["Bo Nix", "Michael Penix Jr."],
-    "RB": ["Jahmyr Gibbs", "Kenneth Walker III", "Tyrone Tracy Jr.", "J.K. Dobbins", "Najee Harris", "Braelon Allen"],
-    "WR": ["Ladd McConkey", "Drake London", "Courtland Sutton", "Keon Coleman"],
+    "RB": ["Jahmyr Gibbs", "Kenneth Walker III", "Tyrone Tracy Jr.", "J.K. Dobbins", "Najee Harris (IR)", "Braelon Allen"],
+    "WR": ["Drake London", "Courtland Sutton", "Ladd McConkey", "Keon Coleman"],
     "TE": ["Harold Fannin Jr."],
     "DST": ["Broncos"],
     "K": ["Harrison Butker"]
   },
 
+  "Hinesward_Jersey": {
+    "QB": ["Lamar Jackson", "Jordan Love"],
+    "RB": ["Omarion Hampton", "Jaylen Warren", "Chase Brown", "Quinshon Judkins"],
+    "WR": ["Justin Jefferson", "DJ Moore", "Calvin Ridley", "Stefon Diggs", "Troy Franklin", "Jayden Reed (IR)"],
+    "TE": ["T.J. Hockenson", "Dalton Kincaid"],
+    "DST": ["Eagles"],
+    "K": ["Cam Little"]
+  },
+
+  "BlueStreak_Bombers": {
+    "QB": ["Patrick Mahomes", "Jared Goff"],
+    "RB": ["Christian McCaffrey", "Alvin Kamara", "Trey Benson", "Aaron Jones (IR)"],
+    "WR": ["Tyreek Hill", "Deebo Samuel", "Tee Higgins", "Rashod Bateman"],
+    "TE": ["Mark Andrews", "Hunter Henry", "Dallas Goedert"],
+    "DST": ["Ravens"],
+    "K": ["Cameron Dicker"]
+  },
+
   "Live_Laugh_Love": {
-    "QB": ["Justin Herbert", "Tua Tagovailoa"],
-    "RB": ["Tony Pollard", "Austin Ekeler", "Brian Robinson Jr.", "Jaydon Blue", "Nick Chubb"],
-    "WR": ["DK Metcalf", "A.J. Brown", "Jakobi Meyers", "Adonai Mitchell"],
+    "QB": ["Justin Herbert", "Tua Tagovailoa", "Jake Browning"],
+    "RB": ["Tony Pollard", "Nick Chubb", "Brian Robinson Jr.", "Jaydon Blue", "Austin Ekeler (IR)"],
+    "WR": ["A.J. Brown", "DK Metcalf", "Jakobi Meyers", "Adonai Mitchell"],
     "TE": ["Tyler Warren", "Tucker Kraft"],
-    "DST": ["Giants"],
-    "K": ["Tyler Bass"]
+    "DST": ["Falcons"],
+    "K": ["Tyler Bass (IR)"]
   }
 
 }
